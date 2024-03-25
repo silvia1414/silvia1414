7854382cd104a8324840ff461fcfe21bddb4e4fe
silvi <silvi@LAPTOP-2Q6MJJBS>
2024-03-25 20:04:41 +0100

    Version 1.0

---
+++ SilviaPrueba/Prueba/silviaprueba/Silviaprueba.java
@@ -0,0 +1,10 @@
+package silviaprueba;
+
+public class Silviaprueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("adios");
+	}
+}
